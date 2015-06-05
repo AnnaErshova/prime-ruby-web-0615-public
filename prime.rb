4c368e9199cf8f0609a10190d2de60480a044001
AnnaErshova <anna.ershova@gmail.com>
2015-06-04 20:45:43 -0400

    all done

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(n) #this has to have a question mark in it apaprently. argh
+  for i in 2..(n-1)
+    if n%i == 0 
+      return false
+    end
+  end
+  true
+end
+
+# number greater than 1 and has no divisors other than 1 and itself --> so the range will start at 2
+# checks if the number 
